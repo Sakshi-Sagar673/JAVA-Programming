ed004362d98a0ac6fbd7456f47062654ac02292b
Sakshi-Sagar673 <85617513+Sakshi-Sagar673@users.noreply.github.com>
2021-06-18 20:34:20 +0530

    Create Reverse.java

---
+++ Basic Programs/Reverse.java
@@ -0,0 +1,20 @@
+import java.util.*;  
+class Reverse{
+	public static void main(String args[]){  
+     int num,rem,rev=0;
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter any number:");
+     num=sc.nextInt();
+	 while(num!=0)
+	 {
+		 rem = num%10;
+		 rev = rev*10 + rem;
+		 num = num/10;
+	 }
+	 System.out.println("Reverse = "+rev);
+	}
+}
+	 
+		 
+		 
+		 
